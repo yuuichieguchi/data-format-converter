1cae625330dfbfe7defbe7f16b0ce36b3ddba89b
Yuuichi <ichi.when@icloud.com>
2025-12-04 22:32:32 +0900

    [add] document.tsx

---
+++ app/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8445672656091773"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
