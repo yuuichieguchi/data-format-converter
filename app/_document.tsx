c9b9f9914e4a94f1ac5d7d2dbddbbc8fc6d4daa6
Yuuichi <ichi.when@icloud.com>
2025-12-04 22:38:59 +0900

    [add] document.tsx

--- app/_document.tsx
+++ app/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <meta name="google-adsense-account" content="ca-pub-8445672656091773" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8445672656091773"
