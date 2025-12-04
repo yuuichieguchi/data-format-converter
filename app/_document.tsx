577aeea474643f240081088a443de895d9dd636f
Yuuichi <ichi.when@icloud.com>
2025-12-04 22:46:47 +0900

    [add] document.tsx

--- app/_document.tsx
+++ app/_document.tsx
@@ -1,20 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head>
         <meta name="google-adsense-account" content="ca-pub-8445672656091773" />
-        <script
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8445672656091773"
           crossOrigin="anonymous"
-        ></script>
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
