c0e33e391478939b37367cc63411604ee1f346c3
Yuuichi <ichi.when@icloud.com>
2025-12-07 01:36:59 +0900

    add: gsc tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export const metadata: Metadata = {
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">📋</text></svg>',
   },
+  verification: {
+    google: "qDjNwUaLfOwddmNI75rPqLANJJNBCRsKXySsNayn_ZI",
+  },
 };
 
 export default function RootLayout({
